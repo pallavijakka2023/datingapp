03027e76b34b637c471df1c193e4ef43dc65c17c
Pallavijakka2023 <pallavijakka2023@gmail.com>
2023-01-29 18:08:57 +0530

    Initial Commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+
+
+  constructor(private http: HttpClient){
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5013/api/user').subscribe({
+      next: response => this.users = response,
+      complete: () => console.log('Request has completed')
+    });
+  }
+  title = 'client';
+  users: any;
+}
